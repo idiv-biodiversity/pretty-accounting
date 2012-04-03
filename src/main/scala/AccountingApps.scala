89071b8bf7be2cf4c7c0a78625f5d0c4599800bd
Christian Krause <christian.krause@idiv.de>
2012-04-03 11:35:37 +0200

    adds system properties to charting apps

--- src/main/scala/AccountingApps.scala
+++ src/main/scala/AccountingApps.scala
@@ -3,22 +3,6 @@ package grid
 import org.jfree.chart.ChartUtilities._
 
 trait AccountingApp extends App with Accounting {
-  def output(name: String)(implicit path: String = sys.env("HOME")) =
-    "%s%s%s-%s.png" format (path, fileSeparator, name, DateTime.now)
-}
-
-object SlotsPerQueue extends AccountingApp {
-  val dataset = (jobs filter wasRunning groupBy { _.queue.get }).perMinute { _.slots }
-  val chart   = createTimeSeriesStackedAreaChart(dataset, "Slots per Queue")
-  saveChartAsPNG(output("slots-per-queue"), chart, 1920, 1080)
-}
-
-object SlotsSequentialVsParallel extends AccountingApp {
-  val dataset = (jobs filter wasRunning groupBy { j =>
-    if (parallel(j)) "parallel" else "sequential"
-  }).perMinute { _.slots }
-  val chart   = createTimeSeriesStackedAreaChart(dataset, "Slots - Sequential vs. Parallel")
-  saveChartAsPNG(output("slots-sequential-vs-parallel"), chart, 1920, 1080)
 }
 
 object EfficiencyByUser extends AccountingApp {
@@ -27,4 +11,45 @@ object EfficiencyByUser extends AccountingApp {
 
 object EfficiencyByGroup extends AccountingApp {
   (efficiencyGroupedBy(jobs) { _.user.gid } toList) sortBy { _._3 } map formatted foreach println
+}
+
+trait ChartingApp extends AccountingApp {
+  /** Returns the name that is used both for the title of the chart and the output file name. */
+  def name: String
+
+  /** Returns the regex used to parse width and height. */
+  protected lazy val geometry = """(\d+)x(\d+)""".r
+
+  lazy val (width,height) = sys.props get "chart.geometry" collect {
+    case geometry(w,h) => w.toInt -> h.toInt
+  } getOrElse 1920 -> 1080
+
+  lazy val output = sys.props get "output.dir" map { dir =>
+    if (dir endsWith fileSeparator) dir substring (0, dir.length - 1) else dir
+  } filter {
+    _.isDirectory
+  } getOrElse {
+    sys.env("HOME")
+  } + "%s%s-%s.png" format (fileSeparator, name, DateTime.now)
+}
+
+object SlotsPerQueue extends ChartingApp {
+  override lazy val name = "Slots per Queue"
+
+  val dataset = jobs filter wasRunning groupBy { _.queue.get } perMinute { _.slots }
+  val chart   = createTimeSeriesStackedAreaChart(dataset, name)
+
+  saveChartAsPNG(output, chart, width, height)
+}
+
+object SlotsSequentialVsParallel extends ChartingApp {
+  override lazy val name = "Slots - Sequential vs. Parallel"
+
+  val dataset = jobs filter wasRunning groupBy { j =>
+    if (parallel(j)) "parallel" else "sequential"
+  } perMinute { _.slots }
+
+  val chart = createTimeSeriesStackedAreaChart(dataset, name)
+
+  saveChartAsPNG(output, chart, width, height)
 }
