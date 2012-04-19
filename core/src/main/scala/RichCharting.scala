8b8d1c672f5e4c6d49071f723e83da798ddaf80e
Christian Krause <christian.krause@idiv.de>
2012-04-19 13:39:50 +0200

    saves charts to PDF documents for better image quality

--- core/src/main/scala/RichCharting.scala
+++ core/src/main/scala/RichCharting.scala
@@ -60,4 +60,52 @@ trait RichCharting extends TypeImports with StaticImports {
     chart.getXYPlot.setDomainAxis(new DateAxis)
     chart
   }
+
+  implicit def pimpedJFreeChart(chart: JFreeChart) = new JFreeChartPimp(chart)
+
+  class JFreeChartPimp(chart: JFreeChart) {
+
+    // -------------------------------------------------------------------
+    // export to pdf
+    // -------------------------------------------------------------------
+
+    import java.awt.geom._
+    import java.io._
+    import com.lowagie.text._
+    import com.lowagie.text.pdf._
+
+    def saveAsPDF(file: File, width: Int, height: Int)
+      (implicit fontMapper: FontMapper = new DefaultFontMapper) {
+      val os = new BufferedOutputStream(new FileOutputStream(file))
+
+      try {
+        writeAsPDF(os, width, height, fontMapper)
+      } finally {
+        os.close()
+      }
+    }
+
+    def writeAsPDF(os: OutputStream, width: Int, height: Int, fontMapper: FontMapper) {
+      val pagesize = new Rectangle(width,height)
+      val document = new Document(pagesize, 50, 50, 50, 50)
+
+      try {
+        val writer = PdfWriter.getInstance(document, os)
+        document.open()
+
+        val cb = writer.getDirectContent
+        val tp = cb.createTemplate(width, height)
+        val g2 = tp.createGraphics(width, height, fontMapper)
+        val r2D = new Rectangle2D.Double(0, 0, width, height)
+
+        chart.draw(g2, r2D)
+        g2.dispose()
+        cb.addTemplate(tp, 0, 0)
+      } finally {
+        document.close()
+      }
+    }
+
+  }
+
 }
