cc60aaf23605e428a5ecc4401f1ac2834bb373a8
Christian Krause <christian.krause@idiv.de>
2012-12-14 15:16:02 +0100

    adds rich date to get quarter of the year

--- src/main/scala/RichTime.scala
+++ src/main/scala/RichTime.scala
@@ -12,14 +12,25 @@ trait RichTime {
     now.withDayOfYear(1).withMillisOfDay(0) to now
   }
 
-  implicit class RichInterval(interval: Interval) {
+  implicit class RichDate(self: DateTime) {
+    def getQuarterOfYear: String = quarter
+
+    def quarter: String = self.getMonthOfYear match {
+      case q1 if ( 1 <= q1) && (q1 <=  3) ⇒ "Q1"
+      case q2 if ( 4 <= q2) && (q2 <=  6) ⇒ "Q2"
+      case q3 if ( 7 <= q3) && (q3 <=  9) ⇒ "Q3"
+      case q4 if (10 <= q4) && (q4 <= 12) ⇒ "Q4"
+    }
+  }
+
+  implicit class RichInterval(self: Interval) {
     import org.joda.time.ReadablePeriod
 
     def by(d: ReadablePeriod) = {
       val coll = collection.mutable.ListBuffer[DateTime]()
 
-      var x = interval.start
-      while (x < interval.end) {
+      var x = self.start
+      while (x < self.end) {
         coll += x
         x += d
       }
@@ -28,9 +39,9 @@ trait RichTime {
     }
   }
 
-  implicit class RichString(s: String) {
-    def toLocalDate = new LocalDate(s)
-    def toDateTime  = new DateTime(s)
+  implicit class RichString(self: String) {
+    def toLocalDate = new LocalDate(self)
+    def toDateTime  = new DateTime(self)
 
     def toLocalDateOption = try {
       Some(toLocalDate)
