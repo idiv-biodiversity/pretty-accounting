e817f7ff8477813739d1336deb26a1d2fe00e16a
Christian Krause <christian.krause@idiv.de>
2012-12-14 15:16:32 +0100

    adds quarter of start categorizer

--- src/main/scala/Categorizing.scala
+++ src/main/scala/Categorizing.scala
@@ -2,24 +2,32 @@ package grid
 
 object Categorizing extends Categorizing
 
-trait Categorizing extends Filtering {
+trait Categorizing extends Filtering with RichTime {
 
   /** Returns a function that categorizes jobs by whether they are sequential or parallel. */
-  val SeqVsPar = (job: Job) ⇒ if (parallel(job)) "parallel".localized else "sequential".localized
+  val SeqVsPar: Job ⇒ String = (job: Job) ⇒ if (parallel(job)) "parallel".localized else "sequential".localized
 
   /** Returns a function that categorizes jobs by their project. */
-  val project = (job: Job) ⇒ job.acl.project
+  val project: Job ⇒ String = (job: Job) ⇒ job.acl.project
 
   /** Returns a function that categorizes jobs by their department. */
   val department: Job ⇒ String = (job: Job) ⇒ job.acl.department
 
   /** Returns a function that categorizes jobs by the month of their submission. */
-  val month_of_submission = (job: Job) ⇒ job.time.submission.toLocalDate.withDayOfMonth(1)
+  val month_of_submission: Job ⇒ LocalDate = (job: Job) ⇒ job.time.submission.toLocalDate.withDayOfMonth(1)
+
+  /** Returns a function that categorizes jobs by the quarterly period they were started. */
+  val quarter_of_start: Job ⇒ String = (job: Job) ⇒ {
+    val quarter = job.time.start.getQuarterOfYear
+    val year    = job.time.start.getYear
+
+    s"$year $quarter"
+  }
 
   /** Returns a function that categorizes jobs by their UID. */
-  val owner = (job: Job) ⇒ job.user.uid
+  val owner: Job ⇒ String = (job: Job) ⇒ job.user.uid
 
   /** Returns a function that categorizes jobs by their GID. */
-  val group = (job: Job) ⇒ job.user.gid
+  val group: Job ⇒ String = (job: Job) ⇒ job.user.gid
 
 }
