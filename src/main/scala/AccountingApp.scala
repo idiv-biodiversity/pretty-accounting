30bfd7867ae79bab39d4175124a23309a88507c0
Christian Krause <christian.krause@idiv.de>
2017-08-25 10:15:48 +0200

    TODO reword CONFIG improves config
    - adds category enum and group by option
    - adds threads/strategy function, this enables a consistent user
      experience regarding parallelization
    - adds progress stream mapping function, this enables a consistent user
      experience regarding progress reporting
    - adds output enum
    
    fixes #5

--- src/main/scala/AccountingApp.scala
+++ src/main/scala/AccountingApp.scala
@@ -3,40 +3,18 @@ package grid
 import cats._
 import cats.implicits._
 
-object AccountingApp {
-  object IntervalExtractor {
-    def unapply(s: String): Option[Interval] =
-      util.Try(new Interval(s)).toOption
-  }
-}
-
+@deprecated("use new config", "")
 trait AccountingApp extends App with Accounting {
+
   /** Returns the name of this app. */
   def name: String
 
   /** Returns the default output file extension. */
   def defaultExtension: String
 
-  implicit def interval: Option[Interval] = sys.props get "grid.accounting.interval" map {
-    _.trim.toLowerCase
-  } collect {
-    case "week"    ⇒ val now = DateTime.now ; (now - 1.week)   to now
-    case "month"   ⇒ val now = DateTime.now ; (now - 1.months) to now
-    case "quarter" ⇒ val now = DateTime.now ; (now - 3.months) to now
-    case "year"    ⇒ val now = DateTime.now ; (now - 1.year)   to now
-    case AccountingApp.IntervalExtractor(interval) ⇒ interval
-  } orElse {
-    sys.props get "grid.accounting.year" flatMap { y ⇒ y.toDateTimeOption } map {
-      year ⇒ year to (year + 1.year)
-    }
-  } orElse {
-    val start = sys.props get "grid.accounting.start" flatMap { _.toDateTimeOption }
-    val end   = sys.props get "grid.accounting.end"   flatMap { _.toDateTimeOption }
+  implicit val conf: Config = Config.empty // TODO remove after migration
 
-    Apply[Option].map2(start, end)(_ to _)
-  }
-
-  def filtered = interval map { interval =>
+  def filtered = conf.interval map { interval =>
     dispatched filter isBetween(interval)
   } getOrElse {
     dispatched
@@ -58,7 +36,7 @@ trait AccountingApp extends App with Accounting {
     outputPath,
     fileSeparator,
     sys.props get "grid.accounting.output.name" getOrElse {
-      name + ("-" + (interval map { _.toString.replaceAll(fileSeparator,"-") } getOrElse { DateTime.now }))
+      name + ("-" + (conf.interval map { _.toString.replaceAll(fileSeparator,"-") } getOrElse { DateTime.now }))
     },
     extension
   )
--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -1,56 +1,209 @@
 package grid
 
-import java.nio.file.{Path, Paths}
+import language.higherKinds
 
 import cats.implicits._
+import enumeratum._
+import fs2._
+import java.nio.file._
 
-case class Config(accountingFiles: Seq[Path] = Nil,
-                  start: Option[DateTime] = None,
-                  end: Option[DateTime] = None,
-                  progress: Boolean = false,
-                  verbose: Boolean = false,
-                  threads: Int = 1) {
+sealed abstract class Category extends EnumEntry
+object Category extends Enum[Category] {
+  val values = findValues
+
+  def isDefinedAt(name: String): Boolean =
+    lowerCaseNamesToValuesMap isDefinedAt name
+
+  final case object Department extends Category
+  final case object Project extends Category
+}
+
+sealed abstract class Output extends EnumEntry
+object Output extends Enum[Output] {
+  val values = findValues
+
+  def isDefinedAt(name: String): Boolean =
+    lowerCaseNamesToValuesMap isDefinedAt name
+
+  final case object Chart extends Output
+  final case object CSV extends Output
+  final case object Table extends Output
+}
+
+final case class Exclude(
+  uids: List[String] = Nil,
+  gids: List[String] = Nil
+)
+
+final case class Config private (
+  val accountingFiles: Seq[Path] = Nil,
+  val start: Option[DateTime] = None,
+  val end: Option[DateTime] = None,
+  val category: Option[Category] = None,
+  val progress: Boolean = false,
+  val resolution: (Int, Int) = (1920, 1080),
+  val output: Output = Output.Table,
+  val verbose: Boolean = false,
+  val threads: Int,
+  val exclude: Exclude,
+  val association: Map[String, Map[String, String]]
+) {
 
   /** Optionally returns the interval from start to end. */
   def interval: Option[Interval] =
     (start, end).mapN(_ to _)
 
+  /** Maps a stream and optionally prints progress.
+    *
+    * Progress is printed every 10000 items. If the `progress` flag is not set,
+    * `s.map(f)` is all that will be done.
+    */
+  def mapWithProgress[F[_], A, B](s: Stream[F, A], file: Path)(f: A => B): Stream[F, B] = {
+    if (!progress) {
+      // simple case, no progress
+      s.map(f)
+    } else {
+      // TODO make niterations costumizable: [--progress [uint32]], scopt can't
+      val niterations = 10000
+
+      // TODO better way to do this? maybe some fs2 way
+      // display index every once in a while
+      s.zipWithIndex map { case (a, index) =>
+        if (index % niterations === 0) {
+          Console.err.println(
+            s"""[${Thread.currentThread.getName}] [i=$index] $file"""
+          )
+        }
+
+        f(a)
+      }
+    }
+  }
+
   /** Returns true if the job has been started between the start and end dates.
     *
-    * If start is not given, the beginning of the universe is assumed. If end is not given, the end
-    * of the universe is assumed.
+    * If start is not given, the beginning of the universe is assumed. If end
+    * is not given, the end of the universe is assumed.
     */
   def startedBetween(job: Job): Boolean =
     start.fold(true)(job.time.start >= _) &&
       end.fold(true)(job.time.start < _)
 
+  /** Returns the amount of threads and the strategy for parallel execution.
+    *
+    * The amount of threads used will be downgraded if there are fewer files
+    * than threads specified via the `--threads` option. If this happens and
+    * the `--verbose` flag is set, there will be a note about it on STDERR.
+    *
+    * {{{
+    * val (threads, strategy) = conf.strategy
+    * implicit val S = strategy
+    * // ...
+    * val stream = fs2.concurrent.join(maxOpen = threads)(streams)
+    * }}}
+    */
+  def strategy: (Int, Strategy) = {
+    // no more than `files.size` threads needed
+    val t = threads min accountingFiles.size
+
+    if (verbose && t < threads) {
+      Console.err.println(
+        // TODO localize
+        s"""there are only $t files, downgrading $threads to $t threads"""
+      )
+    }
+
+    t -> fs2.Strategy.fromFixedDaemonPool(t)
+  }
 }
 
 object Config {
+
+  final case class FromFile(
+    threads: Int,
+    exclude: Exclude,
+    association: Map[String, Map[String, String]]
+  )
+
+  private val fromFile: Option[FromFile] = {
+    val p = Paths.get(scala.util.Properties.userHome, ".pa.conf")
+    pureconfig.loadConfig[FromFile](p).toOption
+  }
+
+  def empty: Config = {
+    Config(
+      threads     = fromFile.map(_.threads    ).getOrElse(1),
+      exclude     = fromFile.map(_.exclude    ).getOrElse(Exclude()),
+      association = fromFile.map(_.association).getOrElse(Map())
+    )
+  }
+
   implicit val pathRead: scopt.Read[Path] =
     scopt.Read.reads(Paths.get(_))
 
   implicit val dateRead: scopt.Read[DateTime] =
     scopt.Read.reads(_.toDateTime)
 
+  private val resolutionRegex = """(\d+)x(\d+)""".r
+
   def parser(name: String) = new scopt.OptionParser[Config](name) {
     head(name, BuildInfo.version)
 
-    opt[Seq[Path]]('f', "file")
-      .required()
-      .valueName("file1,file2...")
-      .action((x, c) => c.copy(accountingFiles = x))
+    arg[Path]("file...")
+      .unbounded()
+      .action((x, c) => c.copy(accountingFiles = c.accountingFiles :+ x))
       .text("accounting files to include")
+
+    note("\noptions:\n")
 
     opt[DateTime]('s', "start")
       .valueName("2016-01-01")
       .action((x, c) => c.copy(start = Some(x)))
-      .text("jobs started at this date")
+      .text("jobs started after this date")
 
     opt[DateTime]('e', "end")
       .valueName("2017-01-01")
       .action((x, c) => c.copy(end = Some(x)))
       .text("jobs started before this date")
+
+    opt[String]('g', "groupby")
+      .valueName("category")
+      .validate(x =>
+        if (Category.lowerCaseNamesToValuesMap.isDefinedAt(x)) { success }
+        else { failure(s"unknown category: $x") }
+      )
+      .action((x, c) => c.copy(category = Category.withNameLowercaseOnlyOption(x)))
+      .text(s"""group by category, categories: ${Category.lowerCaseNamesToValuesMap.keys.mkString(", ")}""")
+
+    note("\nother options:\n")
+
+    opt[String]("output-format")
+      .valueName("output")
+      .validate(x =>
+        if (Output.lowerCaseNamesToValuesMap.isDefinedAt(x)) { success }
+        else { failure(s"unknown output: $x") }
+      )
+      .action((x, c) => c.copy(output = Output.withNameLowercaseOnly(x)))
+      .text(s"""output format, formats: ${Output.lowerCaseNamesToValuesMap.keys.mkString(", ")}""")
+
+    opt[String]("resolution")
+      .valueName("1920x1080")
+      .validate(x =>
+        if (resolutionRegex.unapplySeq(x).isDefined) { success }
+        else { failure(s"invalid resolution: $x") }
+      )
+      .action((x, c) =>
+        x match {
+          case resolutionRegex(width, height) =>
+            c.copy(resolution = (width.toInt, height.toInt))
+        }
+      )
+      .text("use this output resolution for charts")
+
+    opt[Int]("threads")
+      .valueName(sys.runtime.availableProcessors.toString)
+      .action((x, c) => c.copy(threads = x))
+      .text("use at most CPUs for concurrent/parallel processing")
 
     opt[Unit]("progress")
       .action((_, c) => c.copy(progress = true))
@@ -60,11 +213,10 @@ object Config {
       .action((_, c) => c.copy(verbose = true))
       .text("prints what is going on under the hood")
 
-    opt[Int]("threads")
-      .valueName(sys.runtime.availableProcessors.toString)
-      .action((x, c) => c.copy(threads = x))
-      .text("use at most CPUs for concurrent/parallel processing")
-
     help("help").text("prints this usage text")
+
+    version("version")
+
+    note("")
   }
 }
