ceb95ff1fd196264a4910839eb067d09b03e0c70
Christian Krause <christian.krause@idiv.de>
2017-08-25 14:45:48 +0200

    TODO unfinished - misc config output enum
    - not an option yet: adds output enum

--- src/main/scala/config.scala
+++ src/main/scala/config.scala
@@ -4,8 +4,8 @@ import language.higherKinds
 
 import cats.implicits._
 import enumeratum._
-import fs2.{Strategy, Stream}
-import java.nio.file.{Path, Paths}
+import fs2._
+import java.nio.file._
 
 sealed abstract class Category extends EnumEntry
 object Category extends Enum[Category] {
@@ -16,6 +16,18 @@ object Category extends Enum[Category] {
 
   case object Department extends Category
   case object Project extends Category
+}
+
+sealed abstract class Output extends EnumEntry
+object Output extends Enum[Output] {
+  val values = findValues
+
+  def isDefinedAt(name: String): Boolean =
+    lowerCaseNamesToValuesMap isDefinedAt name
+
+  case object Chart extends Output
+  case object CSV extends Output
+  case object Table extends Output
 }
 
 case class Config(accountingFiles: Seq[Path] = Nil,
@@ -32,8 +44,8 @@ case class Config(accountingFiles: Seq[Path] = Nil,
 
   /** Maps a stream and optionally prints progress.
     *
-    * Progress is printed every 10000 items. If the `progress` flag is not set, `s.map(f)` is all
-    * that will be done.
+    * Progress is printed every 10000 items. If the `progress` flag is not set,
+    * `s.map(f)` is all that will be done.
     */
   def mapWithProgress[F[_], A, B](s: Stream[F, A], file: Path)(f: A => B): Stream[F, B] = {
     if (!progress) {
@@ -47,7 +59,9 @@ case class Config(accountingFiles: Seq[Path] = Nil,
       // display index every once in a while
       s.zipWithIndex map { case (a, index) =>
         if (index % niterations === 0) {
-          Console.err.println(s"""[${Thread.currentThread.getName}] [i=$index] $file""")
+          Console.err.println(
+            s"""[${Thread.currentThread.getName}] [i=$index] $file"""
+          )
         }
 
         f(a)
@@ -57,8 +71,8 @@ case class Config(accountingFiles: Seq[Path] = Nil,
 
   /** Returns true if the job has been started between the start and end dates.
     *
-    * If start is not given, the beginning of the universe is assumed. If end is not given, the end
-    * of the universe is assumed.
+    * If start is not given, the beginning of the universe is assumed. If end
+    * is not given, the end of the universe is assumed.
     */
   def startedBetween(job: Job): Boolean =
     start.fold(true)(job.time.start >= _) &&
@@ -66,9 +80,9 @@ case class Config(accountingFiles: Seq[Path] = Nil,
 
   /** Returns the amount of threads and the strategy for parallel execution.
     *
-    * The amount of threads used will be downgraded if there are fewer files than threads specified
-    * via the `--threads` option. If this happens and the `--verbose` flag is set, there will be a
-    * note about it on STDERR.
+    * The amount of threads used will be downgraded if there are fewer files
+    * than threads specified via the `--threads` option. If this happens and
+    * the `--verbose` flag is set, there will be a note about it on STDERR.
     *
     * {{{
     * val (threads, strategy) = conf.strategy
@@ -81,11 +95,12 @@ case class Config(accountingFiles: Seq[Path] = Nil,
     // no more than `files.size` threads needed
     val t = threads min accountingFiles.size
 
-    if (verbose && t < threads)
+    if (verbose && t < threads) {
       Console.err.println(
         // TODO localize
         s"""there are only $t files, downgrading $threads to $t threads"""
       )
+    }
 
     t -> fs2.Strategy.fromFixedDaemonPool(t)
   }
@@ -104,7 +119,7 @@ object Config {
     opt[DateTime]('s', "start")
       .valueName("2016-01-01")
       .action((x, c) => c.copy(start = Some(x)))
-      .text("jobs started at this date")
+      .text("jobs started after this date")
 
     opt[DateTime]('e', "end")
       .valueName("2017-01-01")
@@ -114,8 +129,8 @@ object Config {
     opt[String]('g', "groupby")
       .valueName("category")
       .validate(x =>
-        if (Category.lowerCaseNamesToValuesMap.isDefinedAt(x)) success
-        else failure(s"unknown category: $x")
+        if (Category.lowerCaseNamesToValuesMap.isDefinedAt(x)) { success }
+        else { failure(s"unknown category: $x") }
       )
       .action((x, c) => c.copy(category = Category.withNameLowercaseOnlyOption(x)))
       .text("group by category, categories: department, project")
